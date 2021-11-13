a4a7d255ea8feff224f1e8697540ed4be517af92
Герсон <88345268+Gxrsxn@users.noreply.github.com>
2021-11-13 00:51:30 -0600

    Delete powershell.py

--- powershell.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-comando = "status.ps1"
-lineaPS = "powershell -Executionpolicy ByPass -File "+ comando
-runningProcesses = subprocess.check_output(lineaPS)
-print(runningProcesses.decode('utf-8', 'ignore'))
